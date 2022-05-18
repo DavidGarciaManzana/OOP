116e4bcb5e63fde1e03ec7a6a5c1818c1e9c3671
David <Davidgarciama@outlook.com>
2022-05-18 16:32:54 -0500

    details

--- interactive_dictionary/script.js
+++ interactive_dictionary/script.js
@@ -1,3 +1,18 @@
+//-----------------------------------INSTRUCTIONS
+// Your job is to create a class Dictionary which you can add words to and their entries. Example:
+// >>> let d = new Dictionary();
+//
+// >>> d.newEntry("Apple", "A fruit that grows on trees");
+//
+// >>> console.log(d.look("Apple"));
+// A fruit that grows on trees
+//
+// >>> console.log(d.look("Banana"));
+// Can't find entry for Banana
+
+
+
+
 // -----------------------------------FIRST TRY
 
 // class Dictionary {
